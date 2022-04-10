b34dfa16e526c75a47a36c795e484f4ad07b72de
NguyenCThi <76519661+NguyenCThi@users.noreply.github.com>
2022-04-10 12:28:44 +0700

    thi update gi do

--- src/main/java/com/nguyencthi/shopQuanAo/quanAo/quanAoController.java
+++ src/main/java/com/nguyencthi/shopQuanAo/quanAo/quanAoController.java
@@ -42,44 +42,7 @@ public class quanAoController {
     @Autowired 
     private quanAoServiceImpl qaService;
 
-	@GetMapping("/products/details/{idQuanAo}")
-	public String showDetailsProducts(@PathVariable("idQuanAo") Integer idQuanAo, Model model) {
-
-		// Cập nhập giỏ hàng
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String test = auth.getName();
-		if (test == "anonymousUser") {
-			List<gioHangSession> cartItems = (ArrayList<gioHangSession>) session.getAttribute("cartItems");
-			if (cartItems == null) {
-				cartItems = new ArrayList<gioHangSession>();
-			}
-			List<hangQuanAo> listHQAInNav = hqaRepo.indexShow();
-            List<loaiQuanAo> listLQAInNav = lqaRepo.indexShow1();
-            model.addAttribute("listLQAInNav", listLQAInNav);
-            model.addAttribute("listHQAInNav", listHQAInNav);
-			model.addAttribute("cartItems", cartItems);
-		} else {
-			nguoiDung nguoiDungODAY = repoND.findByEmail(test);
-			List<gioHang> cartItems = cartRepo.findByNguoiDung(nguoiDungODAY.getIdNguoiDung());
-			model.addAttribute("cartItems", cartItems);
-			List<hangQuanAo> listHQAInNav = hqaRepo.indexShow();
-            List<loaiQuanAo> listLQAInNav = lqaRepo.indexShow1();
-            model.addAttribute("listLQAInNav", listLQAInNav);
-            model.addAttribute("listHQAInNav", listHQAInNav);
-		}
-
-		quanAo product = repoQA.findById(idQuanAo).get();
-		List<quanAo> listQA = repoQA.get3RandomItem();
-		model.addAttribute("product", product);
-		model.addAttribute("listQA", listQA);
-		return "detailsQA";
-	}
 	
-	
-	@GetMapping("/products")
-	public String listCategories(Model model) {
-		return showAll(1, model, "giaTien", "asc");
-	}
 	
 	@GetMapping("/products/{pageNo}")
 	public String showAll(@PathVariable(value = "pageNo") int pageNo, Model model, @Param("sortField") String sortField, @Param("sortDir") String sortDir) {
